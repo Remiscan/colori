2a91ca1b4341a9e2e2260b250c038a4f3d4e0c1a
Rémi Scandella <remi.scandella@gmail.com>
2020-08-23 14:23:43 +0200

    Update documentation

--- docs/documentation-js.php
+++ docs/documentation-js.php
@@ -27,6 +27,11 @@
   "l": 0.5,
   "w": 0,
   "bk": 0,
+  "ciel": 0.5429173546502365,
+  "ciea": 80.81243175889557,
+  "cieb": 69.8850744848609,
+  "ciec": 106.83900393835908,
+  "cieh": 0.11347954138219268,
   "name": "red"
 }</code></pre>
 </div>
@@ -41,6 +46,10 @@
   <li data-string="documentation-creer-liste-sl"><?=$Textes->getString('documentation-creer-liste-sl')?></li>
   <li data-string="documentation-creer-liste-wbk"><?=$Textes->getString('documentation-creer-liste-wbk')?></li>
   <li data-string="documentation-creer-liste-a"><?=$Textes->getString('documentation-creer-liste-a')?></li>
+  <li data-string="documentation-creer-liste-ciel"><?=$Textes->getString('documentation-creer-liste-ciel')?></li>
+  <li data-string="documentation-creer-liste-cieab"><?=$Textes->getString('documentation-creer-liste-cieab')?></li>
+  <li data-string="documentation-creer-liste-ciec"><?=$Textes->getString('documentation-creer-liste-ciec')?></li>
+  <li data-string="documentation-creer-liste-cieh"><?=$Textes->getString('documentation-creer-liste-cieh')?></li>
   <li data-string="documentation-creer-liste-name"><?=$Textes->getString('documentation-creer-liste-name')?></li>
 </ul>
 
@@ -51,7 +60,9 @@
 <pre><code class="language-javascript">rosso.hex == '#ff0000'
 rosso.rgb == 'rgb(255, 0, 0)'
 rosso.hsl == 'hsl(0, 100%, 50%)'
-rosso.hwb == 'hwb(0 0% 0%)'</code></pre>
+rosso.hwb == 'hwb(0 0% 0%)'
+rosso.lab == 'lab(54% 81 70)'
+rosso.lch == 'lch(54% 107 41)'</code></pre>
 
 <p data-string="documentation-formats-p2"><?=$Textes->getString('documentation-formats-p2')?></p>
 
@@ -61,7 +72,9 @@ rosso.hwb == 'hwb(0 0% 0%)'</code></pre>
   <pre class="output"><code slot="output" class="language-javascript">colore.hex == '#931c2d4d'
 colore.rgb == 'rgb(147, 28, 45, 0.3)'
 colore.hsl == 'hsl(351, 68%, 34%, 0.3)'
-colore.hwb == 'hwb(351 11% 42% / 0.3)'</code></pre>
+colore.hwb == 'hwb(351 11% 42% / 0.3)'
+colore.lab == 'lab(33% 49 23 / 0.3)'
+colore.lch == 'lch(33% 54 25 / 0.3)'</code></pre>
 </div>
 
 <p data-string="documentation-formats-p3"><?=$Textes->getString('documentation-formats-p3')?></p>
@@ -76,42 +89,13 @@ rosso.hsl == 'hsl(0, 100%, 50%)'
 rosso.hsla == 'hsla(0, 100%, 50%, 1)'
 
 rosso.hwb == 'hwb(0 0% 0%)'
-rosso.hwba == 'hwb(0 0% 0% / 1)'</code></pre>
+rosso.hwba == 'hwb(0 0% 0% / 1)'
 
-<h2 class="titre-partie-docu" data-string="documentation-fusion-titre"><?=$Textes->getString('documentation-fusion-titre')?></h2>
+rosso.lab == 'lab(54% 81 70)'
+rosso.laba == 'lab(54% 81 70 / 1)'
 
-<p data-string="documentation-fusion-p1"><?=$Textes->getString('documentation-fusion-p1')?></p>
-
-<div class="example-code">
-  <pre class="input"><code class="language-javascript">const trasparente = new Colore('rgba(255, 255, 255, .5)');
-const opaco = new Colore('rgb(0, 0, 0)');
-const fusione = Colore.blend(trasparente, opaco);</code></pre>
-
-  <pre class="output"><code slot="output" class="language-javascript">fusione.rgb == 'rgb(128, 128, 128)'</code></pre>
-</div>
-
-<h2 class="titre-partie-docu" data-string="documentation-contraste-titre"><?=$Textes->getString('documentation-contraste-titre')?></h2>
-
-<p data-string="documentation-contraste-p1"><?=$Textes->getString('documentation-contraste-p1')?></p>
-
-<div class="example-code">
-  <pre class="input"><code class="language-javascript">const bianco = new Colore('white');
-const nero = new Colore('black');</code></pre>
-
-  <pre class="output"><code slot="output" class="language-javascript">Colore.contrast(nero, bianco) == 21</code></pre>
-</div>
-
-<p data-string="documentation-contraste-p2"><?=$Textes->getString('documentation-contraste-p2')?></p>
-
-<div class="example-code">
-  <pre class="input"><code class="language-javascript">const sfondo = new Colore('darkred');</code></pre>
-
-  <pre class="output"><code slot="output" class="language-javascript">sfondo.contrastedText() == 'white'</code></pre>
-</div>
-
-<p data-string="documentation-contraste-p3"><?=$Textes->getString('documentation-contraste-p3')?></p>
-
-<pre><code class="language-javascript">sfondo.luminance() == 0.05488967453113127</code></pre>
+rosso.lch == 'lch(54% 107 41)'
+rosso.lcha == 'lch(54% 107 41 / 1)'</code></pre>
 
 <h2 class="titre-partie-docu" data-string="documentation-change-titre"><?=$Textes->getString('documentation-change-titre')?></h2>
 
@@ -163,16 +147,16 @@ nuovoColore.hsl == 'hsl(0, 100%, 10%)'</code></pre>
   <pre class="input"><code class="language-javascript">const nuovoColore = rosso.darken('10%');</code></pre>
 
   <pre class="output"><code slot="output" class="language-javascript">rosso.hsl == 'hsl(0, 100%, 50%)'
-nuovoColore.hsl == 'hsl(0, 100%, 40%)'</code></pre>
+nuovoColore.hsl == 'hsl(0, 100%, 45%)'</code></pre>
 </div>
 
 <p data-string="documentation-change-p4"><?=$Textes->getString('documentation-change-p4')?></p>
 
 <div class="example-code">
-  <pre class="input"><code class="language-javascript">const nuovoColore = rosso.darken('10%', {scale: true});</code></pre>
+  <pre class="input"><code class="language-javascript">const nuovoColore = rosso.darken('10%', {scale: false});</code></pre>
 
   <pre class="output"><code slot="output" class="language-javascript">rosso.hsl == 'hsl(0, 100%, 50%)'
-nuovoColore.hsl == 'hsl(0, 100%, 45%)'</code></pre>
+nuovoColore.hsl == 'hsl(0, 100%, 40%)'</code></pre>
 </div>
 
 <p data-string="documentation-change-p5"><?=$Textes->getString('documentation-change-p5')?></p>
@@ -184,7 +168,7 @@ nuovoColore.hsl == 'hsl(0, 100%, 45%)'</code></pre>
   <li><code class="language-javascript">saturate</code><span data-string="documentation-change-aliases-saturate"><?=$Textes->getString('documentation-change-aliases-saturate')?></span></li>
 </ul>
 
-<p class="h3"><code class="language-javascript">greyscale</code></p>
+<p class="h3"><code class="language-javascript">greyscale</code> / <code class="language-javascript">grayscale</code></p>
 
 <p data-string="documentation-change-p8"><?=$Textes->getString('documentation-change-p8')?></p>
 
@@ -195,7 +179,7 @@ nuovoColore.hsl == 'hsl(0, 100%, 45%)'</code></pre>
 nuovoColore.hsl == 'hsl(0, 0%, 50%)'</code></pre>
 </div>
 
-<h2 class="titre-partie-docu" data-string="documentation-complement-titre"><?=$Textes->getString('documentation-complement-titre')?></h2>
+<p class="h3"><code class="language-javascript">complement</code>, <code class="language-javascript">negative</code> / <code class="language-javascript">invert</code></p>
 
 <p data-string="documentation-complement-p1"><?=$Textes->getString('documentation-complement-p1')?></p>
 
@@ -208,3 +192,56 @@ const negativo = rosa.negative();</code></pre>
 complementare.rgb == 'rgb(194, 255, 245)'
 negativo.rgb == 'rgb(0, 63, 52)'</code></pre>
 </div>
+
+<p class="h3"><code class="language-javascript">blend</code></p>
+
+<p data-string="documentation-fusion-p1"><?=$Textes->getString('documentation-fusion-p1')?></p>
+
+<div class="example-code">
+  <pre class="input"><code class="language-javascript">const trasparente = new Colore('rgba(255, 255, 255, .5)');
+const opaco = new Colore('rgb(0, 0, 0)');
+const fusione = Colore.blend(trasparente, opaco);</code></pre>
+
+  <pre class="output"><code slot="output" class="language-javascript">fusione.rgb == 'rgb(128, 128, 128)'</code></pre>
+</div>
+
+<p data-string="documentation-fusion-p2"><?=$Textes->getString('documentation-fusion-p2')?></p>
+
+<div class="example-code">
+  <pre class="input"><code class="language-javascript">const fusione = opaco.blend(trasparente);</code></pre>
+
+  <pre class="output"><code slot="output" class="language-javascript">fusione.rgb == 'rgb(128, 128, 128)'</code></pre>
+</div>
+
+<p data-string="documentation-fusion-p3"><?=$Textes->getString('documentation-fusion-p3')?></p>
+
+<h2 class="titre-partie-docu" data-string="documentation-comparer-titre"><?=$Textes->getString('documentation-comparer-titre')?></h2>
+
+<p class="h3"><code class="language-javascript">contrast</code></p>
+
+<p data-string="documentation-contraste-p1"><?=$Textes->getString('documentation-contraste-p1')?></p>
+
+<div class="example-code">
+  <pre class="input"><code class="language-javascript">const bianco = new Colore('white');
+const nero = new Colore('black');</code></pre>
+
+  <pre class="output"><code slot="output" class="language-javascript">Colore.contrast(nero, bianco) == 21</code></pre>
+</div>
+
+<p data-string="documentation-contraste-p4"><?=$Textes->getString('documentation-contraste-p4')?></p>
+
+<div class="example-code">
+  <pre class="output"><code slot="output" class="language-javascript">nero.contrast(bianco) == 21</code></pre>
+</div>
+
+<p data-string="documentation-contraste-p2"><?=$Textes->getString('documentation-contraste-p2')?></p>
+
+<div class="example-code">
+  <pre class="input"><code class="language-javascript">const sfondo = new Colore('darkred');</code></pre>
+
+  <pre class="output"><code slot="output" class="language-javascript">sfondo.contrastedText() == 'white'</code></pre>
+</div>
+
+<p data-string="documentation-contraste-p3"><?=$Textes->getString('documentation-contraste-p3')?></p>
+
+<pre><code class="language-javascript">sfondo.luminance() == 0.05488967453113127</code></pre>
--- docs/documentation-php.php
+++ docs/documentation-php.php
@@ -29,6 +29,11 @@
   public 'w' => float 0
   public 'bk' => float 0
   public 'a' => float 1
+  public 'ciel' => float 0.5429173546502365
+  public 'ciea' => float 80.81243175889557
+  public 'cieb' => float 69.8850744848609
+  public 'ciec' => float 106.83900393835908
+  public 'cieh' => float 0.11347954138219268
   public 'name' => string 'red'</code></pre>
 </div>
 
@@ -42,6 +47,10 @@
   <li data-string="documentation-creer-liste-sl"><?=$Textes->getString('documentation-creer-liste-sl')?></li>
   <li data-string="documentation-creer-liste-wbk"><?=$Textes->getString('documentation-creer-liste-wbk')?></li>
   <li data-string="documentation-creer-liste-a"><?=$Textes->getString('documentation-creer-liste-a')?></li>
+  <li data-string="documentation-creer-liste-ciel"><?=$Textes->getString('documentation-creer-liste-ciel')?></li>
+  <li data-string="documentation-creer-liste-cieab"><?=$Textes->getString('documentation-creer-liste-cieab')?></li>
+  <li data-string="documentation-creer-liste-ciec"><?=$Textes->getString('documentation-creer-liste-ciec')?></li>
+  <li data-string="documentation-creer-liste-cieh"><?=$Textes->getString('documentation-creer-liste-cieh')?></li>
   <li data-string="documentation-creer-liste-name"><?=$Textes->getString('documentation-creer-liste-name')?></li>
 </ul>
 
@@ -52,7 +61,9 @@
 <pre><code class="language-php">$rouge->hex() == '#ff0000'
 $rouge->rgb() == 'rgb(255, 0, 0)'
 $rouge->hsl() == 'hsl(0, 100%, 50%)'
-$rouge->hwb() == 'hwb(0 0% 0%)'</code></pre>
+$rouge->hwb() == 'hwb(0 0% 0%)'
+$rouge->lab() == 'lab(54% 81 70)'
+$rouge->lch() == 'lch(54% 107 41)'</code></pre>
 
 <p data-string="documentation-formats-p2"><?=$Textes->getString('documentation-formats-p2')?></p>
 
@@ -62,7 +73,9 @@ $rouge->hwb() == 'hwb(0 0% 0%)'</code></pre>
   <pre class="output"><code slot="output" class="language-php">$couleur->hex() == '#931c2d4d'
 $couleur->rgb() == 'rgb(147, 28, 45, 0.3)'
 $couleur->hsl() == 'hsl(351, 68%, 34%, 0.3)'
-$couleur->hwb() == 'hwb(351 11% 42% / 0.3)'</code></pre>
+$couleur->hwb() == 'hwb(351 11% 42% / 0.3)'
+$couleur->lab() == 'lab(33% 49 23 / 0.3)'
+$couleur->lch() == 'lch(33% 54 25 / 0.3)'</code></pre>
 </div>
 
 <p data-string="documentation-formats-p3"><?=$Textes->getString('documentation-formats-p3')?></p>
@@ -77,42 +90,13 @@ $rouge->hsl() == 'hsl(0, 100%, 50%)'
 $rouge->hsla() == 'hsla(0, 100%, 50%, 1)'
 
 $rouge->hwb() == 'hwb(0 0% 0%)'
-$rouge->hwba() == 'hwb(0 0% 0% / 1)'</code></pre>
+$rouge->hwba() == 'hwb(0 0% 0% / 1)'
 
-<h2 class="titre-partie-docu" data-string="documentation-fusion-titre"><?=$Textes->getString('documentation-fusion-titre')?></h2>
+$rouge->lab() == 'lab(54% 81 70)'
+$rouge->laba() == 'lab(54% 81 70 / 1)'
 
-<p data-string="documentation-fusion-p1"><?=$Textes->getString('documentation-fusion-p1')?></p>
-
-<div class="example-code">
-  <pre class="input"><code class="language-php">$transparente = new Couleur('rgba(255, 255, 255, .5)');
-$opaque = new Couleur('rgb(0, 0, 0)');
-$fusion = Couleur::blend($transparente, $opaque);</code></pre>
-
-  <pre class="output"><code slot="output" class="language-php">$fusione->rgb() == 'rgb(128, 128, 128)'</code></pre>
-</div>
-
-<h2 class="titre-partie-docu" data-string="documentation-contraste-titre"><?=$Textes->getString('documentation-contraste-titre')?></h2>
-
-<p data-string="documentation-contraste-p1"><?=$Textes->getString('documentation-contraste-p1')?></p>
-
-<div class="example-code">
-  <pre class="input"><code class="language-php">$blanc = new Couleur('white');
-$noir = new Couleur('black');</code></pre>
-
-  <pre class="output"><code slot="output" class="language-php">Couleur::contrast($noir, $blanc) == 21</code></pre>
-</div>
-
-<p data-string="documentation-contraste-p2"><?=$Textes->getString('documentation-contraste-p2')?></p>
-
-<div class="example-code">
-  <pre class="input"><code class="language-php">$fond = new Couleur('darkred');</code></pre>
-
-  <pre class="output"><code slot="output" class="language-php">$fond->contrastedText() == 'white'</code></pre>
-</div>
-
-<p data-string="documentation-contraste-p3"><?=$Textes->getString('documentation-contraste-p3')?></p>
-
-<pre><code class="language-php">$fond->luminance() == 0.05488967453113127</code></pre>
+$rouge->lch() == 'lch(54% 107 41)'
+$rouge->lcha() == 'lch(54% 107 41 / 1)'</code></pre>
 
 <h2 class="titre-partie-docu" data-string="documentation-change-titre"><?=$Textes->getString('documentation-change-titre')?></h2>
 
@@ -164,16 +148,16 @@ $nouvelleCouleur->hsl() == 'hsl(0, 100%, 10%)'</code></pre>
   <pre class="input"><code class="language-php">$nouvelleCouleur = $rouge->darken('10%');</code></pre>
 
   <pre class="output"><code slot="output" class="language-php">$rouge->hsl() == 'hsl(0, 100%, 50%)'
-$nouvelleCouleur->hsl() == 'hsl(0, 100%, 40%)'</code></pre>
+$nouvelleCouleur->hsl() == 'hsl(0, 100%, 45%)'</code></pre>
 </div>
 
 <p data-string="documentation-change-p4"><?=$Textes->getString('documentation-change-p4')?></p>
 
 <div class="example-code">
-  <pre class="input"><code class="language-php">$nouvelleCouleur = $rouge->darken('10%', {scale: true});</code></pre>
+  <pre class="input"><code class="language-php">$nouvelleCouleur = $rouge->darken('10%', {scale: false});</code></pre>
 
   <pre class="output"><code slot="output" class="language-php">$rouge->hsl() == 'hsl(0, 100%, 50%)'
-$nouvelleCouleur->hsl() == 'hsl(0, 100%, 45%)'</code></pre>
+$nouvelleCouleur->hsl() == 'hsl(0, 100%, 40%)'</code></pre>
 </div>
 
 <p data-string="documentation-change-p5"><?=$Textes->getString('documentation-change-p5')?></p>
@@ -185,7 +169,7 @@ $nouvelleCouleur->hsl() == 'hsl(0, 100%, 45%)'</code></pre>
   <li><code class="language-php">saturate</code><span data-string="documentation-change-aliases-saturate"><?=$Textes->getString('documentation-change-aliases-saturate')?></span></li>
 </ul>
 
-<p class="h3"><code class="language-php">greyscale</code></p>
+<p class="h3"><code class="language-javascript">greyscale</code> / <code class="language-javascript">grayscale</code></p>
 
 <p data-string="documentation-change-p8"><?=$Textes->getString('documentation-change-p8')?></p>
 
@@ -196,7 +180,7 @@ $nouvelleCouleur->hsl() == 'hsl(0, 100%, 45%)'</code></pre>
 $nouvelleCouleur->hsl() == 'hsl(0, 0%, 50%)'</code></pre>
 </div>
 
-<h2 class="titre-partie-docu" data-string="documentation-complement-titre"><?=$Textes->getString('documentation-complement-titre')?></h2>
+<p class="h3"><code class="language-javascript">complement</code>, <code class="language-javascript">negative</code> / <code class="language-javascript">invert</code></p>
 
 <p data-string="documentation-complement-p1"><?=$Textes->getString('documentation-complement-p1')?></p>
 
@@ -209,3 +193,56 @@ $negative = $rose->negative();</code></pre>
 $complementare->rgb() == 'rgb(194, 255, 245)'
 $negative->rgb() == 'rgb(0, 63, 52)'</code></pre>
 </div>
+
+<p class="h3"><code class="language-javascript">blend</code></p>
+
+<p data-string="documentation-fusion-p1"><?=$Textes->getString('documentation-fusion-p1')?></p>
+
+<div class="example-code">
+  <pre class="input"><code class="language-php">$transparente = new Couleur('rgba(255, 255, 255, .5)');
+$opaque = new Couleur('rgb(0, 0, 0)');
+$fusion = Couleur::blend($transparente, $opaque);</code></pre>
+
+  <pre class="output"><code slot="output" class="language-php">$fusion->rgb() == 'rgb(128, 128, 128)'</code></pre>
+</div>
+
+<p data-string="documentation-fusion-p2-php"><?=$Textes->getString('documentation-fusion-p2-php')?></p>
+
+<div class="example-code">
+  <pre class="input"><code class="language-php">$fusion = $opaque->blend($transparente);</code></pre>
+
+  <pre class="output"><code slot="output" class="language-php">$fusion->rgb() == 'rgb(128, 128, 128)'</code></pre>
+</div>
+
+<p data-string="documentation-fusion-p3-php"><?=$Textes->getString('documentation-fusion-p3-php')?></p>
+
+<h2 class="titre-partie-docu" data-string="documentation-comparer-titre"><?=$Textes->getString('documentation-comparer-titre')?></h2>
+
+<p class="h3"><code class="language-javascript">contrast</code></p>
+
+<p data-string="documentation-contraste-p1"><?=$Textes->getString('documentation-contraste-p1')?></p>
+
+<div class="example-code">
+  <pre class="input"><code class="language-php">$blanc = new Couleur('white');
+$noir = new Couleur('black');</code></pre>
+
+  <pre class="output"><code slot="output" class="language-php">Couleur::contrast($noir, $blanc) == 21</code></pre>
+</div>
+
+<p data-string="documentation-contraste-p4-php"><?=$Textes->getString('documentation-contraste-p4-php')?></p>
+
+<div class="example-code">
+  <pre class="output"><code slot="output" class="language-javascript">$noir->contrast($blanc) == 21</code></pre>
+</div>
+
+<p data-string="documentation-contraste-p2"><?=$Textes->getString('documentation-contraste-p2')?></p>
+
+<div class="example-code">
+  <pre class="input"><code class="language-php">$fond = new Couleur('darkred');</code></pre>
+
+  <pre class="output"><code slot="output" class="language-php">$fond->contrastedText() == 'white'</code></pre>
+</div>
+
+<p data-string="documentation-contraste-p3"><?=$Textes->getString('documentation-contraste-p3')?></p>
+
+<pre><code class="language-php">$fond->luminance() == 0.05488967453113127</code></pre>
