4290f103944aca949257fadbda713213f40649eb
Rémi Scandella <remi.scandella@gmail.com>
2020-08-24 02:02:08 +0200

    Initial work on nav button for mobile

--- index.php
+++ index.php
@@ -570,9 +570,10 @@ while (Couleur::contrast($sectionColor, $bodyColor) < 1.2) {
 
         // Create nav links
         let navHtml = '<ul>';
-        let previousDepth = 0;                         
+        let previousDepth = 0;
+        let depth = 0;
         for (const a of anchors) {
-          const depth = (a.tagName == 'H2') ? 0 : 1;
+          depth = (a.tagName == 'H2') ? 0 : 1;
           if (depth > previousDepth) navHtml += '\n<ul>';
           else if (depth < previousDepth) navHtml += '\n</ul>';
           navHtml += `\n<li><a href="#${getAnchorUrl(a)}">${a.textContent}</a></li>`;
@@ -580,11 +581,25 @@ while (Couleur::contrast($sectionColor, $bodyColor) < 1.2) {
 
           a.outerHTML = `<a id="${getAnchorUrl(a)}" class="anchor-dest"></a>\n${a.outerHTML}`;
         }
-        navHtml += '\n</ul>';
+        while (depth >= 0) {
+          navHtml += '\n</ul>';
+          depth--;
+        }
 
         // Populate links on page
+        const buttonHtml = `
+          <input type="checkbox" id="enable-nav">
+          <label for="enable-nav"><svg height="24" width="24" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" style="fill: var(--h1-color);"/></svg></label>
+        `;
         const aside = document.querySelector('aside.nav-documentation');
-        aside.innerHTML = navHtml;
+        aside.innerHTML = navHtml + buttonHtml;
+
+        // Enable nav button for mobile
+        const navButton = document.querySelector('input#enable-nav');
+        navButton.addEventListener('change', () => {
+          if (navButton.checked) aside.classList.add('on');
+          else aside.classList.remove('on');
+        });
 
         return navHtml;
       }
