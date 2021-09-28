5bfbf3c71fb20217ed1864944e5e346ed164708c
Rémi Scandella <remi.scandella@gmail.com>
2021-09-28 02:13:10 +0200

    Use rollup to build colori.js and .min.js

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import { terser } from 'rollup-plugin-terser';
+
+export default {
+  input: 'src/main.js',
+  output: [
+    {
+      file: 'colori.js',
+      format: 'es'
+    }, {
+      file: 'colori.min.js',
+      format: 'es',
+      plugins: [terser()]
+    }
+  ]
+};
--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  entry: './src/main.js',
-  output: {
-    filename: 'colori.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
