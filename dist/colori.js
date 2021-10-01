21978c0558bdd60dfd634d1fd3bb7f122b9825b9
Rémi Scandella <remi.scandella@gmail.com>
2021-10-01 12:45:41 +0200

    Use deno instead of node

--- dist/colori.js
+++ dist/colori.js
@@ -414,6 +414,10 @@ const Conversions = /*#__PURE__*/Object.freeze({
 
 /** Graph node in a graph that will be traversed by a path finding algorithm. */
 class GraphNode {
+  /**
+   * Builds a graph node from an object.
+   * @param {object} object - An { id, links } object.
+   */
   constructor(object) {
     this.id = object.id;
     this.links = object.links;
@@ -424,26 +428,46 @@ class GraphNode {
   visit(mark = true) { this.visited = mark; }
   unvisit() { this.visited = false; }
   follow(node) { this.predecessorID = node.id; }
+  unfollow() { this.predecessorID = null; }
 }
 
 
 
 /** Graph that will be traversed by a path finding algorithm. */
 class Graph {
+  /**
+   * Builds a graph from an array.
+   * @param {object[]} array - Array of { id, links } objects.
+   */
   constructor(array) {
     this.nodes = array.map(e => new GraphNode(e));
   }
 
+  /**
+   * Finds a node.
+   * @param {string} id - Identifier of the desired node.
+   * @returns {GraphNode} The corresponding node.
+   */
   getNode(id) {
     const node = this.nodes.find(node => node.id === id);
     if (typeof node === 'undefined') throw `Node ${JSON.stringify(id)} does not exist`;
     return node;
   }
 
+  /** Resets the nodes to their starting state. */
   cleanUp() {
-    for (const node of this.nodes) { node.unvisit(); }
+    for (const node of this.nodes) {
+      node.unvisit();
+      node.unfollow();
+    }
   }
 
+  /**
+   * Finds the shortest path between two nodes.
+   * @param {string} startID - Identifier of the first node.
+   * @param {string} endID - Identifier of the last node.
+   * @returns {string[]} An array of node IDs, ordered from first to last along the shortest path.
+   */
   shortestPath(startID, endID) {
     // Source of the math: https://en.wikipedia.org/wiki/Breadth-first_search  
     if (startID === endID) return [];
@@ -487,6 +511,10 @@ class Graph {
     return path.reverse();
   }
 
+  /**
+   * Lists the graph nodes in a topological order.
+   * @returns {GraphNode[]} The array of ordered graph nodes.
+   */
   topologicalOrder() {
     // Source of the math: https://en.wikipedia.org/wiki/Topological_sorting#Depth-first_search
     const orderedList = [];
@@ -494,7 +522,7 @@ class Graph {
 
     const visit = node => {
       if (node.visited === true) return;
-      if (node.visited === 'temp') throw 'The graph is not a directed acyclical graph';
+      if (node.visited === 'temp') throw 'The graph is not a directed acyclic graph';
 
       node.visit('temp'); // Mark visit as temporary to detect if we loop back to this node
       for (const link of node.links) { visit(link); }
@@ -2530,4 +2558,5 @@ class Palette {
   }
 }
 
-export { cssFormats as CSSFormats, Conversions, Palette, Couleur as default };
+export default Couleur;
+export { cssFormats as CSSFormats, Conversions, Palette };
