55e7d2673d789b97a5e4cb073e504a0ff47da33f
Rémi Scandella <remi.scandella@gmail.com>
2021-10-05 23:08:25 +0200

    Log full fs event

--- auto.build.rollup.js
+++ auto.build.rollup.js
@@ -21,7 +21,7 @@ const watcher = Deno.watchFs('./src/ts');
 for await (const event of watcher) {
   // Check if a TypeScript file was modified. Do nothing if not.
   if ([event.paths].flat().filter(path => path.endsWith('.ts')).length === 0) continue;
-  console.log(`File system event: ${event.kind} > ${event.paths}`);
+  console.log(`File system event:`, event);
 
   // Compite TypeScript to JavaScript
   const tsc = Deno.run({ cmd: ['cmd', '/c', 'tsc', 'src/ts/main.ts', '--target', 'esnext', '--module', 'esnext', '--strict'] });
